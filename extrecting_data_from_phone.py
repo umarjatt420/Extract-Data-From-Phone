a879e8d28c2a9de5f2a6b2e11e488cdf870c6257
Umar Jatt <umarjatt0420@gmail.com>
2023-06-28 00:44:02 +0500

    add extrecting data file

---
+++ extrecting_data_from_phone.py
@@ -0,0 +1,15 @@
+import phonenumbers
+from phonenumbers import timezone, geocoder, carrier
+    
+# Parsing String to Phone number
+def get_phone_data(number):
+  phoneNumber = phonenumbers.parse(number)
+  return {
+      "baisc-data": phoneNumber,
+      "time-zone": timezone.time_zones_for_number(phoneNumber),
+      "career": carrier.name_for_number(phoneNumber, 'en'),
+      "is_valid": phonenumbers.is_valid_number(phoneNumber),
+      "is_possible": phonenumbers.is_possible_number(phoneNumber)
+  }
+  
+print(get_phone_data("+923032576030"))
